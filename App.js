8fc2fd0b43afc5f069db301d6035daaa6b729068
Gary Darnell <gdarnell9@gmail.com>
2021-11-16 13:51:43 +0000

    Added dummy data for ten players.

---
+++ App.js
@@ -0,0 +1,42 @@
+//Dummy Data
+
+const player1 = {
+    name: 'player1',
+    value: 4
+}   
+const player2 = {
+    name: 'player2',
+    value: 6
+}   
+const player3 = {
+    name: 'player3',
+    value: 4.8
+}   
+const player4 = {
+    name: 'player4',
+    value: 4.6
+}   
+const player5 = {
+    name: 'player5',
+    value: 4
+}   
+const player6 = {
+    name: 'player6',
+    value: 5
+}   
+const player7 = {
+    name: 'player7',
+    value: 8
+}   
+const player8 = {
+    name: 'player8',
+    value: 7
+}   
+const player9 = {
+    name: 'player9',
+    value: 4.6
+}   
+const player10 = {
+    name: 'player10',
+    value: 7.9
+}   
